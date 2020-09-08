da834d17b6191fd2af985e5987d7d96f6cb60b1b
Rodolfo Ruiz <rpruiz@me.com>
2020-09-08 18:53:37 -0500

    Walls ( # ) and Floors( . )

--- src/game.rs
+++ src/game.rs
@@ -5,6 +5,8 @@ use entity_table::{Entity, EntityAllocator};
 #[derive(Clone, Copy, Debug)]
 pub enum Tile {
     Player,
+    Floor,
+    Wall,
 }
 
 entity_table::declare_entity_module! {
@@ -28,9 +30,10 @@ type SpatialTable = spatial_table::SpatialTable<layers::Layers>;
 pub type Location = spatial_table::Location<Layer>;
 
 pub struct GameState {
-    screen_size: Size,
+    entity_allocator: EntityAllocator,
     components: Components,
     player_entity: Entity,
+    screen_size: Size,
     spatial_table: SpatialTable,
 }
 
@@ -48,9 +51,10 @@ impl GameState {
         let player_entity = entity_allocator.alloc();
         let spatial_table = SpatialTable::new(screen_size);
         let mut game_state = Self {
-            screen_size,
             components,
+            entity_allocator,
             player_entity,
+            screen_size,
             spatial_table,
         };
         game_state.populate(screen_size.to_coord().unwrap() / 2);
@@ -64,10 +68,37 @@ impl GameState {
             .expect("player has no coord component");
         let new_player_coord = player_coord + direction.coord();
         if new_player_coord.is_valid(self.screen_size) {
-            self.spatial_table
+            let dest_layers = self.spatial_table.layers_at_checked(new_player_coord);
+            if dest_layers.character.is_none() && dest_layers.feature.is_none() {
+                self.spatial_table
                 .update_coord(self.player_entity, new_player_coord)
                 .unwrap();
+            }
         }
+    }
+
+    fn populate(&mut self, player_coord: Coord) {
+        self.spawn_player(player_coord);
+        for coord in self.screen_size.coord_iter_row_major() {
+            self.spawn_floor(coord);
+        }
+        self.spawn_wall(player_coord + Coord::new(-1, 2));
+        self.spawn_wall(player_coord + Coord::new(0, 2));
+        self.spawn_wall(player_coord + Coord::new(1, 2));
+    }
+
+    fn spawn_floor(&mut self, coord: Coord) {
+        let entity = self.entity_allocator.alloc();
+        self.spatial_table
+            .update(
+                entity,
+                Location {
+                    coord,
+                    layer: Some(Layer::Floor),
+                },
+            )
+            .unwrap();
+        self.components.tile.insert(entity, Tile::Floor);
     }
 
     fn spawn_player(&mut self, coord: Coord) {
@@ -85,8 +116,17 @@ impl GameState {
             .insert(self.player_entity, Tile::Player);
     }
 
-    fn populate(&mut self, player_coord: Coord) {
-        self.spawn_player(player_coord);
+    fn spawn_wall(&mut self, coord: Coord) {
+        let entity = self.entity_allocator.alloc();
+        self.spatial_table
+            .update(
+                entity, Location {
+                    coord,
+                    layer: Some(Layer::Feature),
+                },
+            )
+            .unwrap();
+        self.components.tile.insert(entity, Tile::Wall);
     }
 
     // Method returns an iterator over EntityToRender for all the entities
